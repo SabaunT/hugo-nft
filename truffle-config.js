d911319e1edb1d21071083369eec3f03376b41c0
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-08-21 08:23:43 +0300

    creating workspace: updating truffle version and some consequent changes

--- truffle-config.js
+++ truffle-config.js
@@ -85,7 +85,7 @@ module.exports = {
   // Configure your compilers. Recommended by open-zeppelin: v0.5.7+commit.6da8b019
   compilers: {
     solc: {
-      version: "0.5.7",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.7",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
